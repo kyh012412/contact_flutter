1b544224ccd1be5cbe2460cb6a60243688bc6d0c
kyh012412 <kyh0124.12@gmail.com>
2024-01-30 17:14:43 +0900

    3박스 레이아웃

--- lib/main.dart
+++ lib/main.dart
@@ -14,33 +14,15 @@ class MyApp extends StatelessWidget {
       // 위젯 짜깁기
       home:Scaffold(
         appBar: AppBar(
-          foregroundColor: Colors.white,
-          backgroundColor: Colors.blue,
-          title: Text('앱임'),
+          title:Text('앱임')
         ),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Icon(Icons.star),
-            Text('안녕'),
-            Icon(Icons.star),
-          ],
-        ),
-        bottomNavigationBar: BottomAppBar(
-          child: SizedBox(
-            height: 100,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Icon(Icons.phone),
-                Icon(Icons.message),
-                Icon(Icons.contact_page)
-              ],
-            ),
+        body: Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            width: double.infinity,height: 50,color: Colors.blue,
           ),
-        ),
-      )      //Container( width: 50, height:50, color:Colors.blue)
+        ), //Container( width: 50, height:50, color:Colors.blue)
+      )
     );
   }
 }
@@ -50,6 +32,13 @@ Icon(Icons.star)
 Icon(Icons.shop) //아이콘이름은 플러터홈페이지에
 Image.asset('assets/로부터의 경로') pubspec.yaml에 이미지 등록이 필요
 Container( width: 50, height:50, color:Colors.blue) //네모박스 넣을 때
+ - 컨테이너에 마진을 모든방향으로 주는법
+    margin: EdgeInsets.all(마진량(숫자)),
+    padding: EdgeInsets.all(20),
+    decoration:
 50이라는 숫자는 단위가 LP임 50LP== 대략 1.2cm
 SizedBox() //네모박스 넣을 때
+
+Center()급 - Align()
+값을 무한히 주는법 double.infinity //부모박스의 크기를 넘지는 않음
  */
